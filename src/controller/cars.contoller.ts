84ff514d87ddf164e1f88ad1337890cecd455303
ratul-sardar <ratulsardar4747@gmail.com>
2026-09-18 16:27:43 +0600

    added api to fetch data of a single car

--- src/controller/cars.contoller.ts
+++ src/controller/cars.contoller.ts
@@ -1,11 +1,20 @@
 import type { RequestHandler } from "express";
+import { ObjectId } from "mongodb";
 import { carsCollection } from "../db/mongodb.db.ts";
 
-// All cars collection
-const carsRes = carsCollection;
-
+// Get all the cars from the collection
 export const getCars: RequestHandler = async (req, res, next) => {
-	const cars = await carsRes.find().toArray();
+	const cars = await carsCollection.find().toArray();
+
+	res.send(cars);
+};
+
+// Get one car from the collection
+export const getCar: RequestHandler = async (req, res, next) => {
+	const { id } = req.params;
+	const carId = id as string; // without the "as string" force type. typescript is giving warning in the ObjectId()
+
+	const cars = await carsCollection.findOne({ _id: new ObjectId(carId) });
 
 	res.send(cars);
 };
--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getCars } from "../controller/cars.contoller.ts";
+import { getCar, getCars } from "../controller/cars.contoller.ts";
 
 const carRouter = Router();
 
 carRouter.get("/", getCars);
+carRouter.get("/:id", getCar);
+carRouter.post("/", async (req, res, next) => res.send("post a car!"));
+carRouter.patch("/", async (req, res, next) => res.send("patch a car!"));
+carRouter.delete("/", async (req, res, next) => res.send("delete a car!"));
 
 export default carRouter;
