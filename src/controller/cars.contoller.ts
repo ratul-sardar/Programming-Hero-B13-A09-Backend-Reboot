f9fd0b379cb1dff7ada85e8f2278520fd7219018
ratul-sardar <ratulsardar4747@gmail.com>
2026-09-18 17:04:53 +0600

    added add-car, update-car and delete-car routes, they are all working fine!

--- src/controller/cars.contoller.ts
+++ src/controller/cars.contoller.ts
@@ -4,17 +4,74 @@ import { carsCollection } from "../db/mongodb.db.ts";
 
 // Get all the cars from the collection
 export const getCars: RequestHandler = async (req, res, next) => {
-	const cars = await carsCollection.find().toArray();
+	try {
+		const cars = await carsCollection.find().toArray();
 
-	res.send(cars);
+		res.send(cars);
+	} catch (err) {
+		next(err);
+	}
 };
 
 // Get one car from the collection
 export const getCar: RequestHandler = async (req, res, next) => {
-	const { id } = req.params;
-	const carId = id as string; // without the "as string" force type. typescript is giving warning in the ObjectId()
+	try {
+		const { id } = req.params;
+		const carId = id as string; // without the "as string" force type. typescript is giving warning in the ObjectId()
 
-	const cars = await carsCollection.findOne({ _id: new ObjectId(carId) });
+		const cars = await carsCollection.findOne({ _id: new ObjectId(carId) });
 
-	res.send(cars);
+		res.send(cars);
+	} catch (err) {
+		next(err);
+	}
+};
+
+// Add a car in the collection
+export const addCar: RequestHandler = async (req, res, next) => {
+	try {
+		const carData = req.body;
+
+		const cars = await carsCollection.insertOne(carData);
+
+		res.send(cars);
+	} catch (err) {
+		next(err);
+	}
+};
+
+// Update a car in the collection
+export const updateCar: RequestHandler = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const carData = req.body;
+		const carId = id as string; // without the "as string" force type. typescript is giving warning in the ObjectId()
+
+		const cars = await carsCollection.updateOne(
+			{ _id: new ObjectId(carId) },
+			{
+				$set: {
+					...carData,
+				},
+			},
+		);
+
+		res.send(cars);
+	} catch (err) {
+		next(err);
+	}
+};
+
+// Delete a car in the collection
+export const deleteCar: RequestHandler = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const carId = id as string; // without the "as string" force type. typescript is giving warning in the ObjectId()
+
+		const cars = await carsCollection.deleteOne({ _id: new ObjectId(carId) });
+
+		res.send(cars);
+	} catch (err) {
+		next(err);
+	}
 };
--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
-import { getCar, getCars } from "../controller/cars.contoller.ts";
+import {
+	addCar,
+	deleteCar,
+	getCar,
+	getCars,
+	updateCar,
+} from "../controller/cars.contoller.ts";
 
 const carRouter = Router();
 
 carRouter.get("/", getCars);
 carRouter.get("/:id", getCar);
-carRouter.post("/", async (req, res, next) => res.send("post a car!"));
-carRouter.patch("/", async (req, res, next) => res.send("patch a car!"));
-carRouter.delete("/", async (req, res, next) => res.send("delete a car!"));
+carRouter.post("/", addCar);
+carRouter.patch("/:id", updateCar);
+carRouter.delete("/:id", deleteCar);
 
 export default carRouter;
